efe6464ba8cf73246919d6b3c16fbf3d83ed5e13
canaanmcarman <canaan.m.carman@gmail.com>
2021-07-12 16:15:10 -0400

    second commit, added readme and main now calls 3 books

---
+++ readme.md
@@ -0,0 +1,3 @@
+Asks the user to input a SKU and gives back a corresponding book object
+from a manual database. 
+Then displays the title, author and description of chosen book.
--- src/BookDB.java
+++ src/BookDB.java
@@ -1,16 +1,16 @@
 import java.util.Scanner;
 
 public class BookDB {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         String SKU;
         Scanner kbd = new Scanner(System.in);
         //BookClass book1 = new BookClass();
 
-
-        System.out.println("Look up book by SKU: ");
-        BookClass book1 = new BookClass(kbd.nextLine());
-
-        System.out.println(book1.getDisplayText());
+        for (int i = 1; i <= 3; i++) {
+            System.out.println("Look up book by SKU: ");
+            BookClass book1 = new BookClass(kbd.nextLine());
+            System.out.println(book1.getDisplayText());
+        }
     }
 }
 
